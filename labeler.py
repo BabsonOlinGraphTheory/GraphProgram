8f227a1c958abe40885e5098dfaeb6571ccd87ef
mafaldaborges <mafaldaborges@mb.(none)>
2014-11-10 22:48:56 -0500

    more labeller

--- labeler.py
+++ labeler.py
@@ -193,3 +193,120 @@ class LMNLabeler(Labeler):
                 memory[0] = [number_of_labels,deepcopy(labels)]
     #            print memory[0]
             return False
+
+    # should work with reals
+    def try_label (spm, vertex, label, maxlabel, labels, constraints, final_check, d_set, memory = [None]):
+        #return [i for i in xrange(len(labels))]
+        #if vertex <= 3:
+        #  bar = "-"*vertex
+        #  print bar + str(color)
+        #yay for verbose output! Make sure not freezing with above code
+     
+        # if pre-colored, then called with this as label, so we are not ignoring it.
+        labels[vertex] = label
+        if not check_vertex_labeling(spm[vertex], vertex, labels, constraints): # only need to pass check_labeling the current row of the SPM.
+            return False
+     
+        # if we're on the last vertex, then check for holes if we are looking for a no-hole coloring
+        if (len(labels)-1 == vertex):
+            return final_check(labels, maxlabel, memory)
+     
+        if labels[vertex+1] != 'NULL':
+             
+            if try_label (spm, vertex+1, labels[vertex+1], maxlabel, labels, constraints, final_check, d_set, memory) != False:
+                return labels
+     
+        else:
+            for l in d_set[0:maxlabel+1]: # This is probably not correct if not using L(2,1) labeling because d_set may not be consecutive
+                #for example, is d_set is [0, 1,3,4], max label of two, can't try to use three.
+    #        for l in range(maxlabel+1):   # TODO: improve efficiency here by selecting which colors we can use?  or only useful for going through list in right order?  won't speed up that much...
+                if try_label(spm, vertex+1, l, maxlabel, labels, constraints, final_check, d_set, memory) != False:
+                    return labels
+            labels[vertex+1] = 'NULL' # reset vertex labeling so that it is not triggered as a pre-configured one next time through the algorithm
+     
+        return False
+     
+    # Works with reals
+    # Creates a matrix with vertices vs. vertices where the intersection an vertex x and vertex y is the shortest path from x to y.
+    # Completes this via a Breadth First Search to find paths for each vertex
+    def shortestpathmatrix(verts,edges):
+        adj = adjacencymatrix(verts,edges)
+    #    verts = self.graph.get_vertices()
+        size = len(verts)
+        for vert in range(size):
+            i = 2
+            A = [vert]
+            B = list(range(size))
+            B.remove(vert)
+            A = neighbors(adj,A,B)
+            for a in A:
+                B.remove(a)
+            while B != [] and A != []:
+                A = neighbors(adj,A,B)
+                for a in A:
+                    adj[vert][a] = i
+                    adj[a][vert] = i
+                    B.remove(a)
+                if A == []:
+                    for b in B:
+                        adj[vert][b] = -1
+                        adj[b][vert] = -1
+                i=i+1
+        return adj
+
+    # Works with reals
+    # Creates a list with the labels of all vertices
+    def labels(verts):
+        return [vert.label for vert in verts]
+
+    # Works with reals
+    def build_d_set(constraints, numverts, i=0, setlist=[], addthis=0):
+        # I believe this creates a listing of all possible labels that could ever be needed for a graph with numvert verrtices
+        myset=set(setlist)
+        if i < len(constraints)-1:
+            for a in range(numverts):
+                myset = build_d_set(constraints, numverts, i+1, myset, addthis+a*constraints[i])
+        else:
+            for a in range(numverts):
+                myset.add(a*constraints[i]+addthis)
+                print("a",a,"constraints", constraints,"i",i,"addthis", addthis)
+                print(a*constraints[i]+addthis)
+        return myset
+
+            return new_matrix
+
+    # Works with reals
+    # Takes in an adjacency matrix and a list "A" and "B" of vertices; returns a list of vertices in B that are adjacent to some vertex in A.
+    def neighbors(adj,A,B):
+        retlist = []
+        for b in B:
+            nlist = adj[b]
+            size = len(nlist)
+            for i in range(size):
+                if nlist[i] == 1 and i in A:
+                    retlist.append(b)
+                    break
+        return retlist
+
+    # Works with reals
+    # Checks the current labeling according to the labeling constraints
+    def check_labeling(graph,constraints):
+        size = len(graph.verts)
+        spm = shortestpathmatrix(graph.verts,graph.edges)
+        cols = labels(verts)
+        maxdist = len(constraints)
+     
+        #checks vertices using the shortest path matrix
+        for i in range(size-1):  # no need to check last vertex, because have checked all other against it.
+            for j in range(i+1,size):  # will have covered interaction of all vertices before this one with itself already
+                dist = spm[i][j]
+                if 0 < dist <= maxdist and cols[i] != 'NULL' and cols[j] != 'NULL':
+                    # only runs if labeling constraints are concerned with this short of path
+                    # and if both vertices are labelled.
+                    # finally, if dist < 0, then vertices are disconnected
+                    coldist = abs(cols[i]-cols[j])
+                    if coldist < constraints[dist-1]: #checks the coloring constraints to make sure it works
+                        return [False,(i,j)]
+                        #TODO: change this to throwing error and catching?
+     
+        return True
