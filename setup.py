7d6b7441f1e75063df88afea3fe10a00b52831e9
Josh Langowitz <jlangowitz@gmail.com>
2015-02-10 22:20:41 -0500

    script for creating windows executables for python 3.4 using py2exe

---
+++ setup.py
@@ -0,0 +1,27 @@
+"""
+This script uses py2exe to create a windows executable version of the program. Works with python 3.4 as of 2/10/2015
+
+Babson/Olin Graph Research Spring 2015
+Josh Langowitz
+"""
+from distutils.core import setup
+import py2exe
+
+windows = {
+	'script':'app.py'
+}
+opts = {
+    'py2exe': {
+        'includes': [
+            'Gui',
+            'generate',
+            'graphmath',
+            'labeler',
+            'labeling',
+            'graph',
+            'view'
+        ],
+        'optimize' : 2
+        }
+}
+setup(windows=[windows], options=opts)
