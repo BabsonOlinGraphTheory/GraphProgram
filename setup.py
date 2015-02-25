fdd253c40f66fa95d693f49bd0b089a3de4cb42f
Josh Langowitz <jlangowitz@gmail.com>
2015-02-24 21:48:06 -0500

    instructions for py2exe

--- setup.py
+++ setup.py
@@ -1,5 +1,6 @@
 """
 This script uses py2exe to create a windows executable version of the program. Works with python 3.4 as of 2/10/2015
+run 'python setup.py py2exe'
 
 Babson/Olin Graph Research Spring 2015
 Josh Langowitz
