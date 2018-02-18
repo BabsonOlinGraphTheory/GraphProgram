b9014ff7b9c0f273440134b2746a14d62f53808e
Serena Chen <serenazychen@gmail.com>
2018-02-18 17:35:55 -0500

    changing the port?

--- src/run_app.py
+++ src/run_app.py
@@ -8,4 +8,5 @@ This script runs the graph app.
 from graphapp import app
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=True, port=port)
