47894aee42947de22b96b75692a4ee3453d91108
lnielsen <louiseenielsen@gmail.com>
2018-02-25 16:44:42 -0500

    deleted vestigial functions

--- forced_graph.py
+++ forced_graph.py
@@ -119,56 +119,7 @@ def exhaustively_test_until_stable(adj):
             print(bitstring)
     return finished_times, un_finished_times
 
-def sample_test_until_stable_too_big(adj, sample_prop=.5):
-    finished_times = {}
-    un_finished_times = {}
-    proto_graphs = []
-    max_num = 1<<len(adj)
-    bitstrings = [bitstring for bitstring in range(1,max_num)]
-    print(len(bitstrings))
-    sample = int(sample_prop*len(bitstrings))
-    chosen_bitstrings = random.sample(bitstrings, sample)
-    for bitstring in chosen_bitstrings: # iterating through all possible bitstrings of length of # of nodes
-        colored = []
-        for idx in range(len(adj)):
-            if bitstring & (1<<idx): # 1<<idx creates a num where the bit at idx is 1, '&' will output 0 if the bitstring does not have a 1 at that idx
-                colored.append(idx)
-        graph_nodes = make_graph(adj, colored_nodes=colored)
-
-        prop_time, is_finished = run_forcing(graph_nodes)
-
-        if is_finished:
-            finished_times[len(colored)] = finished_times.get(len(colored), []) + [prop_time]
-        else:
-            un_finished_times[len(colored)] = un_finished_times.get(len(colored), []) + [prop_time]
-
-        if len(colored) == 0:
-            print(bitstring)
-    return finished_times, un_finished_times
-
-def sample_test_until_stable_too_slow(adj, sample_prop=.5):
-    finished_times = {}
-    un_finished_times = {}
-    proto_graphs = []
-    max_num = 1<<len(adj)
-    for bitstring in range(1, max_num): # iterating through all possible bitstrings of length of # of nodes
-        colored = []
-        for idx in range(len(adj)):
-            if bitstring & (1<<idx): # 1<<idx creates a num where the bit at idx is 1, '&' will output 0 if the bitstring does not have a 1 at that idx
-                colored.append(idx)
-        proto_graphs.append(colored)
-    sample = int(sample_prop*len(proto_graphs))
-    chosen_protographs = random.sample(proto_graphs, sample)
-    for proto_graph in chosen_protographs:
-        prop_time, is_finished = run_forcing(make_graph(adj,colored_nodes=proto_graph))
-
-        if is_finished:
-            finished_times[len(proto_graph)] = finished_times.get(len(proto_graph), []) + [prop_time]
-        else:
-            un_finished_times[len(proto_graph)] = un_finished_times.get(len(proto_graph), []) + [prop_time]
-    return finished_times, un_finished_times
-
-def sample_test_until_stable_current(adj, sample_num=500):
+def sample_test_until_stable(adj, sample_num=500):
     finished_times = {}
     un_finished_times = {}
     proto_graphs = []
